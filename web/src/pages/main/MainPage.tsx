7e65311ab594f8b36fe5bdc02eda3679696e633e
seungdeok <akk9132@naver.com>
2025-04-29 16:10:20 +0900

    refactor: containers 제거

--- web/src/pages/main/MainPage.tsx
+++ web/src/pages/main/MainPage.tsx
@@ -1,7 +1,58 @@
-import { MainContainer } from './containers/MainContainer';
+import { useFilters } from './hooks/useFilters';
+import { FilterView } from './components/FilterView';
+import { ChartView } from './components/ChartView';
+import { ChartListView } from './components/ChartListView';
+import type { ChartItem } from '@web/shared/types/domain';
+import { themeClass } from '@web/common/style/theme.css';
+import { SAMPLE_CHARTS } from '@web/shared/constants/filter';
+import {
+  container,
+  filterArea,
+  chartArea,
+  listArea,
+} from './style.css';
 
 export function MainPage() {
-  return <MainContainer />;
+  const chartItems: ChartItem[] = SAMPLE_CHARTS;
+  const { 
+    timeframe, 
+    setTimeframe, 
+    timeframeError,
+    symbolId,
+    symbol,
+    updateSymbolId,
+    symbolError,
+    symbols,
+  } = useFilters();
+
+  return (
+    <div className={`${themeClass} ${container}`}>
+      <div className={filterArea}>
+        <FilterView
+          timeframe={timeframe}
+          onChangeTimeframe={setTimeframe}
+          timeframeError={timeframeError}
+          symbolId={symbolId}
+          onChangeSymbol={updateSymbolId}
+          symbolError={symbolError}
+          symbols={symbols}
+        />
+      </div>
+      
+      <div className={chartArea}>
+        <ChartView
+          timeframe={timeframe} 
+          symbol={symbol}
+        />
+      </div>
+      
+      <div className={listArea}>
+        <ChartListView
+          chartItems={chartItems}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default MainPage;
