e55e700a20640e07fb058f3f085f2fa1eb9b04c5
seungdeok <akk9132@naver.com>
2025-05-08 15:38:03 +0900

    refactor: route single source로 변환

--- web/src/__tests__/RenderWithProviders.tsx
+++ web/src/__tests__/RenderWithProviders.tsx
@@ -1,26 +1,12 @@
 import { render } from '@testing-library/react';
-import { MainPage } from '@web/pages/main/MainPage';
+import { routes } from '@web/shared/providers/RouterProvider';
 import { RQProvider } from '@web/shared/providers/RQProvider';
 import { RouterProvider, createMemoryRouter } from 'react-router';
 
 export function RenderWithProviders({ route = '/' } = {}) {
   // https://testing-library.com/docs/example-react-router/
   const router = createMemoryRouter(
-    [
-      {
-        path: '/',
-        children: [
-          {
-            index: true,
-            element: <MainPage />,
-          },
-          {
-            path: '*',
-            element: <div>Not Found</div>,
-          },
-        ],
-      },
-    ],
+    routes,
     {
       initialEntries: [route],
     }
