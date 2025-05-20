d615c3d0ce730cc116fe8a464e8c9600c2d3eb56
seungdeok <akk9132@naver.com>
2025-05-20 11:44:09 +0900

    chore: ga4 설정

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
+import { useGARouteTracking } from '@web/shared/hooks/useGARouteTracking';
 import { RQProvider } from '@web/shared/providers/RQProvider';
 import { AppRouterProvider } from '@web/shared/providers/RouterProvider';
 
 function App() {
+  useGARouteTracking();
+
   return (
     <RQProvider>
       <AppRouterProvider />
