b9089677e0ba569ec62fdd4f554aea51e9189560
seungdeok <akk9132@naver.com>
2025-05-23 15:13:56 +0900

    refactor: useSyncExternalStore 인자 수정

--- web/src/shared/hooks/useOnlineStatus.ts
+++ web/src/shared/hooks/useOnlineStatus.ts
@@ -1,20 +1,21 @@
-import { useEffect, useSyncExternalStore } from 'react';
+import { useCallback, useEffect, useSyncExternalStore } from 'react';
 
 /**
  * @param callback 온라인 상태 변경 시 호출할 콜백 함수
  */
 export function useOnlineStatus(callback?: (isOnline: boolean) => void): boolean {
-  const isOnline = useSyncExternalStore<boolean>(
-    cb => {
-      window.addEventListener('online', cb);
-      window.addEventListener('offline', cb);
-      return () => {
-        window.removeEventListener('online', cb);
-        window.removeEventListener('offline', cb);
-      };
-    },
-    () => navigator.onLine
-  );
+  const getSnapshot = useCallback(() => navigator.onLine, []);
+
+  const subscribe = useCallback((cb: () => void) => {
+    window.addEventListener('online', cb);
+    window.addEventListener('offline', cb);
+    return () => {
+      window.removeEventListener('online', cb);
+      window.removeEventListener('offline', cb);
+    };
+  }, []);
+
+  const isOnline = useSyncExternalStore<boolean>(subscribe, getSnapshot);
 
   useEffect(() => {
     callback?.(isOnline);
--- web/src/shared/hooks/useVisibilityChange.ts
+++ web/src/shared/hooks/useVisibilityChange.ts
@@ -1,16 +1,17 @@
-import { useEffect, useSyncExternalStore } from 'react';
+import { useCallback, useEffect, useSyncExternalStore } from 'react';
 
 /**
  * @param callback Visibility 변경 시 호출할 콜백 함수
  */
 export function useVisibilityChange(callback?: (state: 'visible' | 'hidden') => void): 'visible' | 'hidden' {
-  const visibilityState = useSyncExternalStore(
-    cb => {
-      document.addEventListener('visibilitychange', cb);
-      return () => document.removeEventListener('visibilitychange', cb);
-    },
-    () => document.visibilityState
-  );
+  const subscribe = useCallback((cb: () => void) => {
+    document.addEventListener('visibilitychange', cb);
+    return () => document.removeEventListener('visibilitychange', cb);
+  }, []);
+
+  const getSnapshot = useCallback(() => document.visibilityState, []);
+
+  const visibilityState = useSyncExternalStore(subscribe, getSnapshot);
 
   useEffect(() => {
     callback?.(visibilityState);
