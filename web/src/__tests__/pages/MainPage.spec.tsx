a639a740f9a025d1ab34124b737694326771446e
seungdeok <akk9132@naver.com>
2025-06-15 10:48:57 +0900

    test: web ci test 설정

--- web/src/__tests__/pages/MainPage.spec.tsx
+++ web/src/__tests__/pages/MainPage.spec.tsx
@@ -20,38 +20,37 @@ describe('Main 페이지', () => {
       router.navigate('/');
     });
 
-    const filterPeriodLabel = await screen.findByText(/기간 설정/);
+    const filterPeriodLabel = await screen.findByText(/filter.interval/);
     expect(filterPeriodLabel).toBeTruthy();
 
-    const filterPeriodSelect = await screen.findByRole('combobox', { name: /기간 설정/ });
+    const filterPeriodSelect = await screen.findByRole('combobox', { name: /filter.interval/ });
     expect(filterPeriodSelect).toBeTruthy();
 
     // 기본 값 확인
     expect(screen.getByText('5분')).toBeTruthy();
 
     // 값 변경
-    await userEvent.selectOptions(filterPeriodSelect, '1');
-    expect((filterPeriodSelect as HTMLSelectElement).value).toBe('1');
+    await userEvent.selectOptions(filterPeriodSelect, '5');
+    expect((filterPeriodSelect as HTMLSelectElement).value).toBe('5');
   });
 
-  test('2. 필터 > 종목 설정 Select를 클릭하였을 때 문구가 잘 변경되는지 테스트합니다.', async () => {
+  test('2. 필터 > 종목 설정 Select가 없는 경우 빈 메시지가 표시되는지 테스트합니다.', async () => {
     const { router } = renderWithRouter({ initialEntries: ['/'] });
 
     await act(async () => {
       router.navigate('/');
     });
 
-    const filterPeriodLabel = await screen.findByText(/종목 설정/);
-    expect(filterPeriodLabel).toBeTruthy();
+    // 종목 label 확인
+    const symbolLabel = await screen.findByText(/filter.symbol/);
+    expect(symbolLabel).toBeTruthy();
 
-    const filterPeriodSelect = await screen.findByRole('combobox', { name: /종목 설정/ });
-    expect(filterPeriodSelect).toBeTruthy();
+    // 빈 메시지 확인
+    const emptyMessage = screen.getByText(/filter.noSymbol/);
+    expect(emptyMessage).toBeTruthy();
 
-    // 기본 값 확인
-    expect(screen.getByText('종목 선택')).toBeTruthy();
-
-    // 값 변경
-    await userEvent.selectOptions(filterPeriodSelect, 'eth');
-    expect((filterPeriodSelect as HTMLSelectElement).value).toBe('eth');
+    // select가 없는 것을 확인
+    const symbolSelect = screen.queryByRole('combobox', { name: /filter.symbol/ });
+    expect(symbolSelect).toBeNull();
   });
 });
