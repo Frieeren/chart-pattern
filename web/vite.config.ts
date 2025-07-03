bc8dd3d47ffc47e8a7ed7d437c99e2fd9c9e626a
seungdeok <akk9132@naver.com>
2025-07-03 15:49:35 +0900

    chore(web): configure @vitejs/plugin-legacy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,12 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import { compression } from "vite-plugin-compression2";
-import { visualizer } from "rollup-plugin-visualizer";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, loadEnv } from 'vite';
+import { compression } from 'vite-plugin-compression2';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -17,10 +18,10 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 8000,
       proxy: {
-        "/api": {
-          target: "http://localhost:8001",
+        '/api': {
+          target: 'http://localhost:8001',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^api/, "/api"),
+          rewrite: path => path.replace(/^api/, '/api'),
         },
       },
     },
@@ -29,12 +30,12 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         output: {
           manualChunks: (id: string) => {
-            if (id.includes("node_modules")) {
-              if (id.includes("apexcharts")) {
-                return "apexcharts";
+            if (id.includes('node_modules')) {
+              if (id.includes('apexcharts')) {
+                return 'apexcharts';
               }
-              if (id.includes("react-apexcharts")) {
-                return "react-apexcharts";
+              if (id.includes('react-apexcharts')) {
+                return 'react-apexcharts';
               }
             }
           },
@@ -53,9 +54,12 @@ export default defineConfig(({ mode }) => {
         project: env.SENTRY_PROJECT,
         authToken: env.SENTRY_AUTH_TOKEN,
         sourcemaps: {
-          assets: "./dist/**",
-          filesToDeleteAfterUpload: "./dist/**/*.map",
+          assets: './dist/**',
+          filesToDeleteAfterUpload: './dist/**/*.map',
         },
+      }),
+      legacy({
+        targets: ['chrome >= 64', 'safari >= 12'],
       }),
     ],
   };
