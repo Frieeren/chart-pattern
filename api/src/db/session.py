9f1e50a8907ff1f18d36fac3a0ad2d6106ad5733
widse <wngud6012@naver.com>
2025-07-21 00:02:33 +0900

    refactor(api): test environment db session for oas ci

--- api/src/db/session.py
+++ api/src/db/session.py
@@ -7,28 +7,36 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-user = os.getenv("DB_USER")
-passwd = os.getenv("DB_PASSWD")
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-db = os.getenv("DB_NAME")
+is_test = os.getenv("APP_ENV") == "test"
 
-SQLALCHEMY_DATABASE_URL = (
-  f"mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset=utf8mb4"
-)
+if is_test:
+  SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+else:
+  user = os.getenv("DB_USER")
+  passwd = os.getenv("DB_PASSWD")
+  host = os.getenv("DB_HOST")
+  port = os.getenv("DB_PORT")
+  db = os.getenv("DB_NAME")
 
-# 데이터베이스 URL 설정
-if not all([user, passwd, host, port, db]):
-  raise ValueError("데이터베이스 환경 변수가 올바르게 설정되지 않았습니다.")
+  SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset=utf8mb4"
+  )
+
+  # 데이터베이스 URL 설정
+  if not all([user, passwd, host, port, db]):
+    raise ValueError("데이터베이스 환경 변수가 올바르게 설정되지 않았습니다.")
 
 # 데이터베이스 엔진 생성
-engine = create_engine(
-  SQLALCHEMY_DATABASE_URL,
-  pool_size=5,
-  max_overflow=10,
-  pool_timeout=30,
-  pool_recycle=1800,
-)
+if is_test:
+  engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+  engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
+  )
 
 # 세션 로컬 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
